078c1584b25e64800d856ba48e082cc55e1a68a8
Jinjin Wang <jinjinwang02@gmail.com>
2022-04-24 13:57:46 +0100

    add test data

---
+++ src/data.ts
@@ -0,0 +1,377 @@
+export const data = {
+  title: 'Jane Doe',
+  contactDetails: [
+    '123 Sunday Road, London, ABC DEF',
+    '+44 0123456789',
+    'janeDoe@gmail.com',
+  ],
+  mainSections: [
+    {
+      heading: 'Profile',
+      direction: 'column',
+      body: [
+        [
+          {
+            style: 'normal',
+            content:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo odio dictum nunc imperdiet, quis porttitor elit dignissim. Praesent mattis dapibus libero, sed pharetra leo bibendum fringilla.',
+          },
+        ],
+      ],
+    },
+    {
+      heading: 'Employment',
+      direction: 'column',
+      body: [
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Company name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Job title',
+                },
+                {
+                  style: 'normal',
+                  content: 'Location',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Key word, key word, key word',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Proin ullamcorper maximus leo. Integer vitae ante at odio tristique pellentesque. Nulla elementum ante nulla, quis interdum magna tempus eget. Cras elementum tortor eu nisi bibendum, bibendum laoreet justo tincidunt. Quisque tincidunt tincidunt dui eu sollicitudin. Nam interdum quis leo eu fringilla. Maecenas facilisis molestie bibendum. Aenean sed elementum ex, a ultricies mauris. Nunc est quam, sagittis ut quam a, dictum finibus dolor. Fusce consectetur augue porttitor, efficitur nulla dictum, dapibus magna. Pellentesque pharetra, sapien id imperdiet porta, ligula dui tristique nulla, nec vestibulum nulla nibh volutpat lectus. Nulla rutrum imperdiet euismod.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Company name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Job title',
+                },
+                {
+                  style: 'normal',
+                  content: 'Location',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Key word, key word, key word',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Proin ullamcorper maximus leo. Integer vitae ante at odio tristique pellentesque. Nulla elementum ante nulla, quis interdum magna tempus eget. Cras elementum tortor eu nisi bibendum, bibendum laoreet justo tincidunt. Quisque tincidunt tincidunt dui eu sollicitudin. Nam interdum quis leo eu fringilla. Maecenas facilisis molestie bibendum. Aenean sed elementum ex, a ultricies mauris. Nunc est quam, sagittis ut quam a, dictum finibus dolor. Fusce consectetur augue porttitor, efficitur nulla dictum, dapibus magna. Pellentesque pharetra, sapien id imperdiet porta, ligula dui tristique nulla, nec vestibulum nulla nibh volutpat lectus. Nulla rutrum imperdiet euismod.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Company name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Job title',
+                },
+                {
+                  style: 'normal',
+                  content: 'Location',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Key word, key word, key word',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Proin ullamcorper maximus leo. Integer vitae ante at odio tristique pellentesque. Nulla elementum ante nulla, quis interdum magna tempus eget. Cras elementum tortor eu nisi bibendum, bibendum laoreet justo tincidunt. Quisque tincidunt tincidunt dui eu sollicitudin. Nam interdum quis leo eu fringilla. Maecenas facilisis molestie bibendum. Aenean sed elementum ex, a ultricies mauris. Nunc est quam, sagittis ut quam a, dictum finibus dolor. Fusce consectetur augue porttitor, efficitur nulla dictum, dapibus magna. Pellentesque pharetra, sapien id imperdiet porta, ligula dui tristique nulla, nec vestibulum nulla nibh volutpat lectus. Nulla rutrum imperdiet euismod.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: 'Education',
+      direction: 'column',
+      body: [
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'University name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Major',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Key word, key word, key word',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'University name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Major',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Key word, key word, key word',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: 'Certification',
+      direction: 'column',
+      body: [
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo odio dictum nunc imperdiet, quis porttitor elit dignissim. Praesent mattis dapibus libero, sed pharetra leo bibendum fringilla.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: 'Volunteer work',
+      direction: 'column',
+      body: [
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Name',
+                },
+                {
+                  style: 'normal',
+                  content: 'Time',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content: 'Role',
+                },
+                {
+                  style: 'normal',
+                  content: 'Location',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo odio dictum nunc imperdiet, quis porttitor elit dignissim. Praesent mattis dapibus libero, sed pharetra leo bibendum fringilla.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      heading: 'Skills',
+      direction: 'row',
+      body: [
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Category 1',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Curabitur pharetra sem quis aliquam tincidunt. Donec id tortor vitae nibh maximus blandit id quis urna. In lacinia ac dui sed maximus. Phasellus ac ullamcorper nibh. Proin volutpat risus sit amet erat feugiat, sit amet congue enim tempor. In scelerisque urna orci, eu rhoncus ante consequat a.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Category 2',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Curabitur pharetra sem quis aliquam tincidunt. Donec id tortor vitae nibh maximus blandit id quis urna. In lacinia ac dui sed maximus. Phasellus ac ullamcorper nibh. Proin volutpat risus sit amet erat feugiat, sit amet congue enim tempor. In scelerisque urna orci, eu rhoncus ante consequat a.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          section: [
+            {
+              row: [
+                {
+                  style: 'bold',
+                  content: 'Category 3',
+                },
+              ],
+            },
+            {
+              row: [
+                {
+                  style: 'normal',
+                  content:
+                    'Curabitur pharetra sem quis aliquam tincidunt. Donec id tortor vitae nibh maximus blandit id quis urna. In lacinia ac dui sed maximus. Phasellus ac ullamcorper nibh. Proin volutpat risus sit amet erat feugiat, sit amet congue enim tempor. In scelerisque urna orci, eu rhoncus ante consequat a.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};

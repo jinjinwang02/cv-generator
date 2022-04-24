511c84b3ea51da40b4fc7999c1aa5e302dbde3e4
Jinjin Wang <jinjinwang02@gmail.com>
2022-04-24 17:41:02 +0100

    generate all available CV links on home page

--- cv-pdf/src/pages/index.tsx
+++ cv-pdf/src/pages/index.tsx
@@ -1,19 +1,59 @@
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-const CVDocument = dynamic(() => import('../components/CVDocument'), {
-  ssr: false,
-});
+import sanityClient from '../lib/sanity';
+import { CvType } from '../types/sanity';
 
-const Home: NextPage = () => (
-  <div>
-    <Head>
-      <title>CV generator</title>
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    <CVDocument />
-  </div>
-);
+type Slug = CvType['slug'];
+
+type Props = {
+  slugs: string[];
+};
+
+const Home: NextPage<Props> = ({ slugs }: Props) => {
+  const { isFallback } = useRouter();
+
+  if (isFallback) return <div>Loading...</div>;
+
+  return (
+    <>
+      <Head>
+        <title></title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <div className='homepage'>
+        <h1>CVs:</h1>
+        <ul>
+          {slugs.map((slug) => (
+            <li key={slug}>
+              <Link href={`cv/${slug}/`}>
+                <a>{slug}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await sanityClient.fetch<{ slug: Slug }[]>(
+    `*[_type == 'cv'] { slug }`
+  );
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  const slugs = data.map(({ slug }) => slug.current);
+
+  return {
+    props: { slugs },
+  };
+};
 
 export default Home;

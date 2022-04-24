21f8c65a88cc0bbc1941089bee49e81762e603ca
Jinjin Wang <jinjinwang02@gmail.com>
2022-04-24 17:58:14 +0100

    refine font size

--- cv-pdf/src/components/CVDocument.tsx
+++ cv-pdf/src/components/CVDocument.tsx
@@ -13,12 +13,6 @@ import Prata from '../public/fonts/Prata-Regular.ttf';
 import EurostileBold from '../public/fonts/Eurostile-Bold.ttf';
 import EuroStile from '../public/fonts/Eurostile-Regular.ttf';
 
-Font.register({ family: 'Prata', src: Prata });
-Font.register({
-  family: 'Eurostile',
-  fonts: [{ src: EuroStile }, { src: EurostileBold, fontWeight: 700 }],
-});
-
 type Props = {
   page: CvType;
 };
@@ -27,86 +21,6 @@ const MAIN_SECTION_GAP = 12;
 const SECTION_VERTICAL_GAP = 8;
 const SECTION_HORIZONTAL_GAP = 12;
 const CONTACT_DETAIL_GAP = 8;
-
-const styles = StyleSheet.create({
-  viewer: {
-    height: '100vh',
-    width: '80vw',
-    borderWidth: 0,
-  },
-  page: {
-    padding: 30,
-    fontFamily: 'Eurostile',
-    fontSize: 10,
-    letterSpacing: 0.4,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
-  pageHeader: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignContent: 'flex-start',
-    justifyContent: 'space-between',
-  },
-  pageHeaderTitle: {
-    fontFamily: 'Prata',
-    fontSize: 36,
-  },
-  pageHeaderContactDetails: {
-    maxWidth: 80,
-  },
-  contactDetailsGap: {
-    marginTop: CONTACT_DETAIL_GAP,
-  },
-  section: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignContent: 'flex-start',
-  },
-  sectionColumn: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  sectionHeading: {
-    flex: 1,
-    fontSize: 12,
-    letterSpacing: 0,
-    fontWeight: 700,
-    textTransform: 'uppercase',
-    maxWidth: 140,
-  },
-  sectionBodyFlexColumn: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    marginBottom: MAIN_SECTION_GAP,
-  },
-  sectionBodyFlexRow: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: MAIN_SECTION_GAP,
-  },
-  sectionBodyFlexColumnGap: {
-    marginBottom: SECTION_VERTICAL_GAP,
-  },
-  sectionBodyRow: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  sectionBodyRowTextBold: {
-    letterSpacing: 0,
-    fontWeight: 700,
-  },
-  sectionBodyFlexRowColumns: {
-    flex: 1,
-  },
-  sectionBodyFlexRowGap: {
-    marginRight: SECTION_HORIZONTAL_GAP,
-  },
-});
 
 const Row = ({ fragments }: { fragments: RowFragmentType['fragments'] }) => (
   <View style={styles.sectionBodyRow}>
@@ -160,41 +74,135 @@ const HorizontalSection = ({ body }: { body: RowsType[] }) => (
   </View>
 );
 
-const PdfDocument = ({ page }: Props): JSX.Element => (
-  <PDFViewer style={styles.viewer}>
-    <Document
-      title='CV_Jane_Doe'
-      author='Jane Doe'
-      subject='CV'
-      creator='Jane Doe'
-      producer='Jane Doe'
-    >
-      <Page size='A4' style={styles.page}>
-        <View style={styles.pageHeader}>
-          <Text style={styles.pageHeaderTitle}>{page.name}</Text>
-          <View style={styles.pageHeaderContactDetails}>
-            {page.contactDetails.map((detail) => (
-              <Text key={detail} style={styles.contactDetailsGap}>
-                {detail}
-              </Text>
+const PdfDocument = ({ page }: Props): JSX.Element => {
+  const { name, contactDetails, mainContent } = page;
+  return (
+    <PDFViewer style={styles.viewer}>
+      <Document
+        title={`${name} CV`}
+        author={name}
+        subject='CV'
+        creator={name}
+        producer={name}
+      >
+        <Page size='A4' style={styles.page}>
+          <View style={styles.pageHeader}>
+            <Text style={styles.pageHeaderTitle}>{name}</Text>
+            <View style={styles.pageHeaderContactDetails}>
+              {contactDetails.map((detail) => (
+                <Text key={detail} style={styles.contactDetailsGap}>
+                  {detail}
+                </Text>
+              ))}
+            </View>
+          </View>
+          <View>
+            {mainContent.map(({ heading, body, direction }, index) => (
+              <View key={index} style={styles.section}>
+                <Text style={styles.sectionHeading}>{heading}</Text>
+                {direction === 'column' ? (
+                  <VerticalSection body={body} />
+                ) : (
+                  <HorizontalSection body={body} />
+                )}
+              </View>
             ))}
           </View>
-        </View>
-        <View>
-          {page.mainContent.map(({ heading, body, direction }, index) => (
-            <View key={index} style={styles.section}>
-              <Text style={styles.sectionHeading}>{heading}</Text>
-              {direction === 'column' ? (
-                <VerticalSection body={body} />
-              ) : (
-                <HorizontalSection body={body} />
-              )}
-            </View>
-          ))}
-        </View>
-      </Page>
-    </Document>
-  </PDFViewer>
-);
+        </Page>
+      </Document>
+    </PDFViewer>
+  );
+};
+
+Font.register({ family: 'Prata', src: Prata });
+Font.register({
+  family: 'Eurostile',
+  fonts: [{ src: EuroStile }, { src: EurostileBold, fontWeight: 700 }],
+});
+
+const styles = StyleSheet.create({
+  viewer: {
+    height: '100vh',
+    width: '100vw',
+    borderWidth: 0,
+  },
+  page: {
+    padding: '30px 30px 22px',
+    fontFamily: 'Eurostile',
+    fontSize: 9.5,
+    letterSpacing: 0.3,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  pageHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'flex-start',
+    justifyContent: 'space-between',
+    marginBottom: MAIN_SECTION_GAP,
+  },
+  pageHeaderTitle: {
+    fontFamily: 'Prata',
+    fontSize: 36,
+  },
+  pageHeaderContactDetails: {
+    maxWidth: 80,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  contactDetailsGap: {
+    marginTop: CONTACT_DETAIL_GAP,
+  },
+  section: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'flex-start',
+  },
+  sectionColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  sectionHeading: {
+    flex: 1,
+    fontSize: 12,
+    letterSpacing: 0,
+    fontWeight: 700,
+    textTransform: 'uppercase',
+    maxWidth: 140,
+  },
+  sectionBodyFlexColumn: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: MAIN_SECTION_GAP,
+  },
+  sectionBodyFlexRow: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: MAIN_SECTION_GAP,
+  },
+  sectionBodyFlexColumnGap: {
+    marginBottom: SECTION_VERTICAL_GAP,
+  },
+  sectionBodyRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  sectionBodyRowTextBold: {
+    letterSpacing: 0,
+    fontWeight: 700,
+    lineHeight: 1.15,
+  },
+  sectionBodyFlexRowColumns: {
+    flex: 1,
+  },
+  sectionBodyFlexRowGap: {
+    marginRight: SECTION_HORIZONTAL_GAP,
+  },
+});
 
 export default PdfDocument;
